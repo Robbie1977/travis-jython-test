73c0a26221c6924f7550f1a777a559467bf0e335
David Osumi-Sutherland <dosumis@gmail.com>
2015-11-19 15:47:06 +0000

    Update call_brain.py

--- call_brain.py
+++ call_brain.py
@@ -2,5 +2,5 @@ from uk.ac.ebi.brain.core import Brain
 
 b = Brain()
 b.learn("http://purl.obolibrary.org/obo/pato.owl")
-b.getSubClasses('Thing', 0)
-print "PATO has %s classes" % len(list(b))
+sc = b.getSubClasses('Thing', 0)
+print "PATO has %s classes" % len(list(sc))
