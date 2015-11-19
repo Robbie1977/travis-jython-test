352e570826e435ec0cf6adc846893411e6f19cc1
David Osumi-Sutherland <dosumis@gmail.com>
2015-11-19 15:32:36 +0000

    Update call_brain.py

--- call_brain.py
+++ call_brain.py
@@ -1,6 +1,6 @@
 from uk.ac.ebi.brain.core import Brain
 
 b = Brain()
-b.learn("http:purl.obo.obolibrary.org/obo/pato.owl")
+b.learn("http://purl.obo.obolibrary.org/obo/pato.owl")
 b.getSubClasses("Thing", 0)
 print len(b)
